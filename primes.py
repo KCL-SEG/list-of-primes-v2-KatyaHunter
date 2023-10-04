6d452fac88c6c00a876c65b754a2b67920e31906
Ekaterina Hunter <ekaterina.hunter@kcl.ac.uk>
2023-10-04 14:24:16 +0100

    Calculate the number of primes

--- primes.py
+++ primes.py
@@ -2,5 +2,30 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+	list = []
+	possPrime = 2
+	numPrimesFound = 0
+	isPrime = True
+
+	while numPrimesFound != number_of_primes:
+		isPrime = True
+	
+		if possPrime == 2:
+			isPrime = True
+		
+		else:
+			for i in range (2, possPrime):
+				if possPrime % i ==0:
+					isPrime = False
+
+		if isPrime == True:
+			list.append(possPrime)
+			numPrimesFound += 1
+
+		possPrime += 1
+
+	return list
+
+
+
+
